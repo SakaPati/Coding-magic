cb379f7fa3c3cbdbee20c08e9b4cbed268ec2ebc
Denis Karatyha <karatygad@gmail.com>
2025-04-04 22:30:16 +0200

    немного поправил

--- src/js/calculate-time.js
+++ src/js/calculate-time.js
@@ -1,6 +1,10 @@
-document.getElementById('calculate-btn').addEventListener('click', function () {
-  const input = document.getElementById('number').value;
-  const resultElement = document.getElementById('result');
+const refs = {
+  calculateBtn: document.querySelector('.calculate-time-btn'),
+};
+
+refs.calculateBtn.addEventListener('click', () => {
+  const input = document.querySelector('.time-input').value;
+  const resultElement = document.querySelector('.calcResult');
   const minutes = parseFloat(input);
 
   if (!isNaN(minutes) && minutes >= 0) {
@@ -10,3 +14,16 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     resultElement.textContent = 'Будь ласка, введіть коректне число';
   }
 });
+
+// document.getElementById('calculate-btn').addEventListener('click', function () {
+//   const input = document.getElementById('number').value;
+//   const resultElement = document.getElementById('result');
+//   const minutes = parseFloat(input);
+
+//   if (!isNaN(minutes) && minutes >= 0) {
+//     const hours = minutes / 60;
+//     resultElement.textContent = `${minutes} хвилин = ${hours.toFixed(2)} годин`;
+//   } else {
+//     resultElement.textContent = 'Будь ласка, введіть коректне число';
+//   }
+// });
